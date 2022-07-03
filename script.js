8abaad125937acbb068c749b71a096b8b3e3286a
mattrichor <matt.a.geyer@gmail.com>
2022-07-02 23:18:19 -0400

    sorted out a workaround for the refreshing access token problem (I think...) it is bad practice for deployable apps bc it auto gets it and is in the bones of the site, but for now it should be fine. not like Im going to deploy this large scale until I have a strava login page anyway

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 const clientSecret = `25d5efd65d8b5cab2e461ebe98e3117b76766e8e`
-const accessToken = `8a15d84d3f5dce03a5fd1e3eabf95ed314b0b117`
+let accessToken = `8a15d84d3f5dce03a5fd1e3eabf95ed314b0b117`
+let starredSegmentsLink = `https://www.strava.com/api/v3/segments/starred?page=1&per_page=30&access_token=${accessToken}`
 const refreshToken = `34a4515d34fcddd543ed7a03b8e572397a6386b7`
 const clientId = `89151`
 
 let segmentList = document.querySelector('.segment-list')
+
+//functionality for user logins so I don't have to repeatedly refresh access code
+const authLink = 'https://www.strava.com/oauth/token'
+
+const getLoggedInAthleteStarredSegments = (res) => {
+  let starredSegmentsLink = `https://www.strava.com/api/v3/segments/starred?page=1&per_page=30&access_token=${res.access_token}`
+}
+
+const reAuthorize = () => {
+  fetch(authLink, {
+    method: 'post',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`
+    },
+
+    body: JSON.stringify({
+      client_id: `${clientId}`,
+      client_secret: `${clientSecret}`,
+      refresh_token: `${refreshToken}`,
+      grant_type: `refresh_token`
+    })
+  })
+    .then((res) => res.json())
+    .then((res) => getLoggedInAthleteStarredSegments(res))
+}
+
+reAuthorize()
 
 // const config = {
 //   headers: { Authorization: `Bearer ${accessToken}` }
@@ -23,15 +53,12 @@ let segmentList = document.querySelector('.segment-list')
 
 // const authCode = `c4570843c740249df095b45d9bd1ccd457bd58e8`
 
-// const APIURL =
 const inputBar = document.getElementById('search-input')
 const searchButton = document.getElementById('search')
 
 searchButton.addEventListener('click', async () => {
   let userInput = inputBar.value
-  const response = await axios.get(
-    `https://www.strava.com/api/v3/segments/starred?page=1&per_page=30&access_token=${accessToken}`
-  )
+  const response = await axios.get(starredSegmentsLink)
   for (let i = 0; i < response.data.length; i++) {
     const segmentName = response.data[i].name
     const listItem = document.createElement('li')
@@ -41,25 +68,26 @@ searchButton.addEventListener('click', async () => {
   }
 })
 
+//*** STRAVA'S JS CODE ON THE API SITE ***/
 // let StravaApiV3 = 'strava_api_v3'
-// let defaultClient = StravaApiV3.ApiClient.instance
+//   let defaultClient = StravaApiV3.ApiClient.instance
 
-// // Configure OAuth2 access token for authorization: strava_oauth
-// const strava_oauth = defaultClient.authentications[`${authCode}`]
-// strava_oauth.accessToken = `${accessToken}`
+//   // Configure OAuth2 access token for authorization: strava_oauth
+//   const strava_oauth = defaultClient.authentications[`${authCode}`]
+//   strava_oauth.accessToken = `${accessToken}`
 
-// const api = new StravaApiV3.SegmentsApi()
+//   const api = new StravaApiV3.SegmentsApi()
 
-// const opts = {
-//   page: 56, // {Integer} Page number. Defaults to 1.
-//   perPage: 56 // {Integer} Number of items per page. Defaults to 30.
-// }
-
-// const callback = function (error, data, response) {
-//   if (error) {
-//     console.error(error)
-//   } else {
-//     console.log('API called successfully. Returned data: ' + data)
+//   const opts = {
+//     page: 56, // {Integer} Page number. Defaults to 1.
+//     perPage: 56 // {Integer} Number of items per page. Defaults to 30.
 //   }
-// }
-// api.getLoggedInAthleteStarredSegments(opts, callback)
+
+//   const callback = function (error, data, response) {
+//     if (error) {
+//       console.error(error)
+//     } else {
+//       console.log('API called successfully. Returned data: ' + data)
+//     }
+//   }
+//   api.getLoggedInAthleteStarredSegments(opts, callback)
