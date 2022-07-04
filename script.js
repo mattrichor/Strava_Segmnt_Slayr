62f91ba3fa049f0286685fd5774b698ac4836aaf
mattrichor <matt.a.geyer@gmail.com>
2022-07-04 12:21:49 -0400

    Created table for segment list on search that accurately returns info and categorizes with correct formatting. Created Segment class to keep it simpler, and because I needed some more practice with OOP :)

--- script.js
+++ script.js
@@ -6,23 +6,61 @@ const clientId = `89151`
 
 let segmentList = document.querySelector('.segment-list')
 
-//functionality for user logins so I don't have to repeatedly refresh access code
-const authLink = 'https://www.strava.com/oauth/token'
+const timeToReadable = (time) => {
+  if (time == undefined) {
+    return 'No Segment Efforts'
+  } else {
+    let min = Math.floor(Math.abs(time))
+    let sec = Math.floor(Math.abs(time) % 60)
+    return (
+      (min < 10 ? '0' : '') +
+      `${(min / 60).toFixed(0)} min ` +
+      (sec < 10 ? '0' : '') +
+      `${sec} sec`
+    )
+  }
+}
+
+class Segment {
+  constructor(segName, segDistance, segGrade, segClimbCat, segPrTime) {
+    this.segName = segName
+    this.segDistance = `${(segDistance * 0.000621371).toFixed(2)} mi`
+    this.segGrade = `${segGrade}%`
+    this.segClimbCat = segClimbCat
+    this.segPrTime = timeToReadable(segPrTime)
+  }
+}
 
 const getLoggedInAthleteStarredSegments = (res) => {
   let starredSegmentsLink = `https://www.strava.com/api/v3/segments/starred?page=1&per_page=30&access_token=${res.access_token}`
+
   searchButton.addEventListener('click', async () => {
     let userInput = inputBar.value
     const response = await axios.get(starredSegmentsLink)
     for (let i = 0; i < response.data.length; i++) {
-      const segmentName = response.data[i].name
-      const listItem = document.createElement('li')
+      let segment = new Segment(
+        response.data[i].name,
+        response.data[i].distance,
+        response.data[i].average_grade,
+        response.data[i].climb_category,
+        response.data[i].pr_time
+      )
 
-      listItem.innerHTML = `${segmentName}`
+      console.log(segment)
+      const listItem = document.createElement('tr')
+      //   console.log(response.data[i])
+      listItem.innerHTML = `<th>${segment.segName}</th>
+      <th>${segment.segDistance}</th>
+      <th>${segment.segGrade}</th>
+      <th>${segment.segClimbCat}</th>
+      <th>${segment.segPrTime}</th>`
       segmentList.append(listItem)
     }
   })
 }
+
+//functionality for user logins so I don't have to repeatedly refresh access code. Huge thanks to everyone online with OAuth tutorials for helping me parse this out!
+const authLink = 'https://www.strava.com/oauth/token'
 
 const reAuthorize = () => {
   fetch(authLink, {
@@ -46,47 +84,13 @@ const reAuthorize = () => {
 
 reAuthorize()
 
-// const config = {
-//   headers: { Authorization: `Bearer ${accessToken}` }
-// }
-
-//www.strava.com/oauth/token?client_id=89151&client_secret=25d5efd65d8b5cab2e461ebe98e3117b76766e8e&code=2aa2e231942117f3b5751b2e72b62491a3799522&grant_type=authorization_code
-
-// const bodyParams = {
-//   key: 'value'
-// }
-
-// axios.post(
-//   `https://www.strava.com/api/v3/athlete/activities`,
-//   bodyParams,
-//   config
-// )
-
-// const authCode = `c4570843c740249df095b45d9bd1ccd457bd58e8`
-
 const inputBar = document.getElementById('search-input')
 const searchButton = document.getElementById('search')
 
-//*** STRAVA'S JS CODE ON THE API SITE ***/
-// let StravaApiV3 = 'strava_api_v3'
-//   let defaultClient = StravaApiV3.ApiClient.instance
-
-//   // Configure OAuth2 access token for authorization: strava_oauth
-//   const strava_oauth = defaultClient.authentications[`${authCode}`]
-//   strava_oauth.accessToken = `${accessToken}`
-
-//   const api = new StravaApiV3.SegmentsApi()
-
-//   const opts = {
-//     page: 56, // {Integer} Page number. Defaults to 1.
-//     perPage: 56 // {Integer} Number of items per page. Defaults to 30.
+//   {
+//     segmName: response.data[i].name,
+//     segDistance: response.data[i].distance,
+//     segGradient: response.data[i].average_grade,
+//     segClimbCat: response.data[i].climb_category,
+//     segPrTime: response.data[i].pr_time
 //   }
-
-//   const callback = function (error, data, response) {
-//     if (error) {
-//       console.error(error)
-//     } else {
-//       console.log('API called successfully. Returned data: ' + data)
-//     }
-//   }
-//   api.getLoggedInAthleteStarredSegments(opts, callback)
